01203936c843dc1fb34266469cb80b9ba2f3b341
Julius Lee <saywhat717@gmail.com>
2016-01-31 04:31:44 -0500

    Hope I don't break anything.

--- ShootyShot/Source/ShootyShot/Private/FoodSpawner.cpp
+++ ShootyShot/Source/ShootyShot/Private/FoodSpawner.cpp
@@ -10,7 +10,7 @@ AFoodSpawner::AFoodSpawner()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	USceneComponent 
+	//USceneComponent 
 
 }
 
